efc9bc00f6a9294772034ca3e8bd62e7525791fd
safin777 <sayedislamsafin7770555@gmail.com>
2021-02-03 23:21:31 +0600

    Complete installation and Home componenet added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home'
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Home></Home>
         </a>
       </header>
     </div>
